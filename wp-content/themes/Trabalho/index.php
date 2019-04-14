7d371ec35797d3a13c6c8dc200778e480aabe1e0
RenanCecchin <renan.cecchin@hotmail.com>
2019-04-14 15:06:32 -0300

    docker-compose.yml

---
+++ wp-content/themes/Trabalho/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TrabalhoPSO</title>
+  <style>
+  header{
+    background-color: #B32140;
+    padding: 1px;
+    text-align: center;
+    font-size: 35px;
+    color: #FF8A75;
+  }
+
+
+
+  </style>
+</head>
+
+<body bgcolor="#FFA08F">
+  <header>
+    <h1>Pratica de sistemas operacionais</h1>
+    <p>(TRABALHO)</p>
+    <p>Renan de Siqueira Cecchin</p> 
+  </header>
+  <p><center>Email:renan.cecchin@hotmail.com</center></p>
+  <center>
+  <img src="https://i.pinimg.com/originals/91/e9/dc/91e9dc377c5ab0f2dfd1075486aecde2.gif" ></center>
+</body>
+
+</html>
